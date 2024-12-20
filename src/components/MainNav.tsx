e38079fea39a30961bd097e2204034f7ff29ff76
hiroki <haohuirixia851@gmail.com>
2024-12-20 15:13:18 +0900

    build mobile nav

--- src/components/MainNav.tsx
+++ src/components/MainNav.tsx
@@ -1,16 +1,23 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import { Button } from "./ui/button";
+import UsernameMenu from "./UsernameMenu";
 
 const MainNav = () => {
-  const { loginWithRedirect } = useAuth0();
+  const { loginWithRedirect, isAuthenticated } = useAuth0();
   return (
-    <Button
-      variant="ghost"
-      className="font-bold text-blue-400 hover:bg-blue-400 hover:text-white duration-300 ease-in-out"
-      onClick={async () => await loginWithRedirect()}
-    >
-      Log in
-    </Button>
+    <span>
+      {isAuthenticated ? (
+        <UsernameMenu />
+      ) : (
+        <Button
+          variant="ghost"
+          className="font-bold outline outline-2 outline-blue-400 rounded-sm text-blue-400 hover:text-white hover:bg-blue-400 duration-300 ease-in-out"
+          onClick={async () => await loginWithRedirect()}
+        >
+          Let's get Started
+        </Button>
+      )}
+    </span>
   );
 };
 
