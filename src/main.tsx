52c411d310e0ac0ac5fd0db310d52b2044232066
hiroki <haohuirixia851@gmail.com>
2024-12-20 18:29:27 +0900

    set up create user api

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,24 @@ import "./global.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import AppRoutes from "./AppRoutes";
 import Auth0ProviderWithnavigate from "./auth/Auth0ProviderWithnavigate";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Router>
-      <Auth0ProviderWithnavigate>
-        <AppRoutes />
-      </Auth0ProviderWithnavigate>
+      <QueryClientProvider client={queryClient}>
+        <Auth0ProviderWithnavigate>
+          <AppRoutes />
+        </Auth0ProviderWithnavigate>
+      </QueryClientProvider>
     </Router>
   </StrictMode>
 );
