cd9bdb84d1e46bd67749dd0cd14643809036c995
hiroki <haohuirixia851@gmail.com>
2024-12-23 19:01:09 +0900

    fix image bug

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,3 +1,8 @@
+// src/pages/HomePage.tsx
+import ellipseBullet from "../assets/bulletpoint/Ellipse.png";
+import person1 from "../assets/users/person-1.jpeg";
+import person2 from "../assets/users/person-2.jpeg";
+
 const HomePage = () => {
   return (
     <main className="flex justify-center flex-col min-h-full px-3 py-3 pt-10 pb-20 md:pt-20 md:px-20">
@@ -8,12 +13,13 @@ const HomePage = () => {
           Want to improve
           <span className="text-sky-400"> your speaking skills</span>??
         </h1>
+
         <div>
           <ul className="mb-5">
-            <li className="flex flex-row items-center ">
+            <li className="flex flex-row items-center">
               <div className="mr-1">
                 <img
-                  src="/src/assets/bulletpoint/Ellipse.png"
+                  src={ellipseBullet}
                   width={7}
                   height={7}
                   alt="blue bullet point"
@@ -26,7 +32,7 @@ const HomePage = () => {
             <li className="flex flex-row items-center">
               <div className="mr-1">
                 <img
-                  src="/src/assets/bulletpoint/Ellipse.png"
+                  src={ellipseBullet}
                   width={7}
                   height={7}
                   alt="blue bullet point"
@@ -37,7 +43,7 @@ const HomePage = () => {
             <li className="flex flex-row items-center">
               <div className="mr-1">
                 <img
-                  src="/src/assets/bulletpoint/Ellipse.png"
+                  src={ellipseBullet}
                   width={7}
                   height={7}
                   alt="blue bullet point"
@@ -53,12 +59,12 @@ const HomePage = () => {
       </div>
 
       <div className="flex flex-col justify-between my-8">
-        <h2 className="font-bold text-2xl	md:text-4xl mb-5">
+        <h2 className="font-bold text-2xl md:text-4xl mb-5">
           Have you ever thought about these?
         </h2>
         <div className="bg-gray-50 px-8 py-4 rounded-md mb-3 flex items-center">
           <img
-            src="/src/assets/users/person-1.jpeg"
+            src={person1}
             width={60}
             height={60}
             alt="A woman complaining"
@@ -67,14 +73,13 @@ const HomePage = () => {
           <div className="flex-1 order-1 md:order-2 text-left">
             <p>
               I&#39;m taking Japanese classes from tutors <br />
-              but the classes are boring and want to have more casual
-              conversation.
+              but the classes are boring and I want more casual conversation.
             </p>
           </div>
         </div>
         <div className="bg-gray-50 px-8 py-4 rounded-md mb-3 flex items-center">
           <img
-            src="/src/assets/users/person-2.jpeg"
+            src={person2}
             width={60}
             height={60}
             alt="A woman complaining"
@@ -82,12 +87,13 @@ const HomePage = () => {
           />
           <div className="flex-1 order-1 md:order-2 text-left">
             <p>
-              I&#39;m looking for a language partner on a language exchange app
-              but it&#39;s a pain to send messages to random people
+              I&#39;m looking for a language partner on a language exchange app,
+              but it&#39;s a pain to message random people.
             </p>
           </div>
         </div>
       </div>
+
       <div className="mt-5">
         <h2 className="text-2xl md:text-4xl font-bold pb-5">
           If you have <br />
