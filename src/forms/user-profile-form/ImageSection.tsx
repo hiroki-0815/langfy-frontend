c777863df69aa816629f420f105a40e751e63ce3
hiroki <haohuirixia851@gmail.com>
2024-12-26 10:05:02 +0900

    build image section

--- src/forms/user-profile-form/ImageSection.tsx
+++ src/forms/user-profile-form/ImageSection.tsx
@@ -1,5 +1,4 @@
 import {
-  FormControl,
   FormDescription,
   FormField,
   FormItem,
@@ -9,38 +8,43 @@ import { Input } from "@/components/ui/input";
 import { useFormContext } from "react-hook-form";
 
 const ImageSection = () => {
-  const { control } = useFormContext();
+  const { control, watch } = useFormContext();
+
+  const existingImageUrl = watch("imageUrl");
+
   return (
     <div className="space-y-2">
-      <div>
-        <h2 className=" text-2xl font-bold">Image</h2>
-        <FormDescription>
-          Add an image that will be displayed on the search results. Adding a
-          new image will overwrite the existing one.
-        </FormDescription>
-        <div className="flex flex-col gap-8 w-[50%]">
-          <FormField
-            control={control}
-            name="imageFile"
-            render={({ field }) => (
-              <FormItem>
-                <FormControl>
-                  <Input
-                    className="bg-white"
-                    type="file"
-                    accept=".jpg, .jpeg, .png"
-                    onChange={(event) =>
-                      field.onChange(
-                        event.target.files ? event.target.files[0] : null
-                      )
-                    }
-                  />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
+      <h2 className="text-2xl font-bold">Image</h2>
+      <FormDescription>
+        Add an image that will be displayed on the search results. Adding a new
+        image will overwrite the existing one.
+      </FormDescription>
+      <div className="flex flex-col items-start gap-2 md:w-[50%]">
+        {existingImageUrl && (
+          <img
+            src={existingImageUrl}
+            className="rounded-full object-cover h-[60px] w-[60px]"
           />
-        </div>
+        )}
+        <FormField
+          control={control}
+          name="imageFile"
+          render={({ field }) => (
+            <FormItem>
+              <Input
+                className="bg-white"
+                type="file"
+                accept=".jpg, .jpeg, .png"
+                onChange={(event) =>
+                  field.onChange(
+                    event.target.files ? event.target.files[0] : null
+                  )
+                }
+              />
+              <FormMessage />
+            </FormItem>
+          )}
+        />
       </div>
     </div>
   );
