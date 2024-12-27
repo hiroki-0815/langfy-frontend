ab38ff3546570423f9ea2029669bf5d0891ab61f
hiroki <haohuirixia851@gmail.com>
2024-12-27 17:59:47 +0900

    add padding

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ import MainNav from "./MainNav";
 
 const Header = () => {
   return (
-    <div className="border-b-2 shadow-sm py-6">
+    <div className="border-b-2 shadow-sm py-6 px-2">
       <div className="container mx-auto flex justify-between items-center">
         <Link
           to="/"
