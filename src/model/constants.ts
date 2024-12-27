07af26f90926052d45afd6a8f91ffcf904f12a23
hiroki <haohuirixia851@gmail.com>
2024-12-27 11:17:01 +0900

    change types

--- src/model/constants.ts
+++ src/model/constants.ts
@@ -1,4 +1,5 @@
 export const GENDERS = ["male", "female"] as const;
+
 export const NATIONALITIES = [
   "Japan",
   "The United States",
@@ -11,6 +12,16 @@ export const NATIONALITIES = [
   "China",
   "Brazil",
 ] as const;
+
 export const LANGUAGES = ["English", "Japanese", "Chinese", "Spanish", "French"] as const;
+
 export const FLUENCY_LEVELS = ["beginner", "intermediate", "advanced"] as const;
+
 export const MOTIVATIONS = ["wanna chat", "wanna call"] as const;
+
+// Type Definitions
+export type Nationality = typeof NATIONALITIES[number];
+export type Language = typeof LANGUAGES[number];
+export type Gender = typeof GENDERS[number];
+export type FluencyLevel = typeof FLUENCY_LEVELS[number];
+export type Motivation = typeof MOTIVATIONS[number];
--- src/model/types.ts
+++ src/model/types.ts
@@ -1,45 +1,4 @@
-export const NATIONALITIES = [
-  "Japan",
-  "The United States",
-  "Canada",
-  "United Kingdom",
-  "India",
-  "Germany",
-  "France",
-  "Australia",
-  "China",
-  "Brazil",
-] as const;
-
-export const LANGUAGES = [
-  "English",
-  "Japanese",
-  "Chinese",
-  "Spanish",
-  "French",
-] as const;
-
-export const GENDERS = [
-  "male",
-  "female",
-] as const;
-
-export const FLUENCY_LEVELS = [
-  "beginner",
-  "intermediate",
-  "advanced",
-] as const;
-
-export const MOTIVATIONS = [
-  "wanna chat",
-  "wanna call",
-] as const;
-
-export type Nationality = typeof NATIONALITIES[number];
-export type Language = typeof LANGUAGES[number];
-export type Gender = typeof GENDERS[number];
-export type FluencyLevel = typeof FLUENCY_LEVELS[number];
-export type Motivation = typeof MOTIVATIONS[number];
+import { FluencyLevel, Gender, Language, Motivation, Nationality } from "./constants";
 
 export type User = {
   name: string;
