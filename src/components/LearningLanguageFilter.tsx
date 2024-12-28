6f100a00036e99646dbd22346aca14bb164713a7
hiroki <haohuirixia851@gmail.com>
2024-12-28 18:56:57 +0900

    build filter

---
+++ src/components/LearningLanguageFilter.tsx
@@ -0,0 +1,33 @@
+import { LANGUAGES } from "@/model/constants";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+
+type Props = {
+  onChange: (language: string) => void;
+};
+
+const LearningLanguageFilter = ({ onChange }: Props) => {
+  return (
+    <div>
+      <DropdownMenu>
+        <DropdownMenuTrigger className="w-full text-left rounded-full bg-gray-100 px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-200">
+          Learning
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="bg-white border border-gray-200 rounded-lg shadow-lg">
+          <DropdownMenuItem onClick={() => onChange("")}>All</DropdownMenuItem>
+          {LANGUAGES.map((language) => (
+            <DropdownMenuItem key={language} onClick={() => onChange(language)}>
+              {language}
+            </DropdownMenuItem>
+          ))}
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  );
+};
+
+export default LearningLanguageFilter;
