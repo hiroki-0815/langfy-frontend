ac9c26a8624e59153015b80649053f9707280c3c
hiroki <haohuirixia851@gmail.com>
2024-12-28 18:56:17 +0900

    build filter

---
+++ src/components/GenderFilter.tsx
@@ -0,0 +1,33 @@
+import { GENDERS } from "@/model/constants";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+
+type Props = {
+  onChange: (gender: string) => void;
+};
+
+const GenderFilter = ({ onChange }: Props) => {
+  return (
+    <div>
+      <DropdownMenu>
+        <DropdownMenuTrigger className="w-full text-left rounded-full bg-gray-100 px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-200">
+          Gender
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="bg-white border border-gray-200 rounded-lg shadow-lg">
+          <DropdownMenuItem onClick={() => onChange("")}>All</DropdownMenuItem>
+          {GENDERS.map((gender) => (
+            <DropdownMenuItem key={gender} onClick={() => onChange(gender)}>
+              {gender.charAt(0).toUpperCase() + gender.slice(1)}
+            </DropdownMenuItem>
+          ))}
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  );
+};
+
+export default GenderFilter;
