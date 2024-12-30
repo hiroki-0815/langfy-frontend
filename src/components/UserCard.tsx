14e86c83f5871cc55c084c43e6f8a475c501c9f6
hiroki <haohuirixia851@gmail.com>
2024-12-30 18:17:23 +0900

    add reset button

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -3,14 +3,14 @@ import { useState } from "react";
 
 type UserCardProps = {
   name: string;
-  gender: string;
+  gender?: string;
   age: number;
   nativeLanguage: string;
-  countryOrigin: string;
+  countryOrigin?: string;
   selfIntroduction?: string;
   imageUrl: string;
   motivation: string;
-  country: string;
+  country?: string;
   learningLanguage: string;
 };
 
@@ -51,11 +51,13 @@ const UserCard = ({
           />
           <div>
             <h2 className="text-sm md:text-xl font-bold mr-3">{name}</h2>
-            <span
-              className={`px-2 py-1 rounded-full text-[12px] ${badgeColor}`}
-            >
-              {age}
-            </span>
+            {gender && (
+              <span
+                className={`px-2 py-1 rounded-full text-[12px] ${badgeColor}`}
+              >
+                {age}
+              </span>
+            )}
           </div>
         </div>
         <button className="px-2 py-1 font-bold outline outline-2 outline-blue-400 bg-white rounded-full text-blue-400 hover:text-white hover:bg-blue-400 duration-300 ease-in-out text-[12px]">
@@ -73,22 +75,29 @@ const UserCard = ({
             </span>
           </div>
           <div className="flex flex-col gap-2">
-            <span className="text-[10px] bg-orange-100 text-orange-600 py-[2px] px-[4px] rounded-xl">
-              Lives: {country}
-            </span>
-            <span className="text-[10px] bg-red-100 text-red-600 py-[2px] px-[4px] rounded-xl">
-              From: {countryOrigin}
-            </span>
-          </div>
-          <div className="flex flex-col gap-2">
             <span className="text-[10px] bg-purple-100 text-purple-600 py-[2px] px-[4px] rounded-xl">
               Learning: {learningLanguage}
             </span>
+            {countryOrigin && (
+              <div className="flex flex-col gap-2">
+                <span className="text-[10px] bg-red-100 text-red-600 py-[2px] px-[4px] rounded-xl">
+                  From: {countryOrigin}
+                </span>
+              </div>
+            )}
+          </div>
+          <div className="flex flex-col gap-2">
+            {country && (
+              <div className="flex flex-col gap-2">
+                <span className="text-[10px] bg-orange-100 text-orange-600 py-[2px] px-[4px] rounded-xl">
+                  Lives: {country}
+                </span>
+              </div>
+            )}
           </div>
         </div>
       </div>
-
-      <div className="mt-4 hidden md:block">
+      <div className="mt-4">
         <h3 className="font-semibold">Self-Introduction</h3>
         <p className="text-gray-700 mt-2 break-words">
           {showFullIntroduction ? selfIntroduction : shortenedIntroduction}
