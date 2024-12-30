dd31b73f8631a325786956e428a4a0fb62ff012e
hiroki <haohuirixia851@gmail.com>
2024-12-30 18:17:04 +0900

    add reset button

--- src/components/LanguageFilterSidebar.tsx
+++ src/components/LanguageFilterSidebar.tsx
@@ -30,7 +30,9 @@ const LanguageFilterSidebar = ({
             <Label
               htmlFor={lang}
               className={`flex flex-1 items-center cursor-pointer text-sm rounded-full px-4 py-2 mb-2 font-semibold gap-2 bg-white hover:text-blue-400 hover:shadow-sm ${
-                isChecked ? "bg-blue-400 text-white" : "border border-slate-300"
+                isChecked
+                  ? "border border-blue-600 text-blue-600"
+                  : "border border-slate-300"
               }`}
             >
               {isChecked && <Check size={20} strokeWidth={3} />}
