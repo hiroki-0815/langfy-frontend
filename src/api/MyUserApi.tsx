eb0cc7a8dce20b97953939b0c2f325194abbe466
hiroki <haohuirixia851@gmail.com>
2025-01-04 16:24:58 +0900

    make chat real time

--- src/api/MyUserApi.tsx
+++ src/api/MyUserApi.tsx
@@ -1,3 +1,4 @@
+// src/api/MyUserApi.ts
 import { User } from "@/model/types";
 import { useAuth0 } from "@auth0/auth0-react";
 import { useMutation, useQuery } from "react-query";
@@ -19,7 +20,6 @@ export const useGetMyUser = () => {
       method: "GET",
       headers: {
         Authorization: `Bearer ${accessToken}`,
-        "Content-Type": "application/json",
       },
     });
 
@@ -95,7 +95,6 @@ export const useUpdateUser = () => {
     if (!response.ok) {
       throw new Error("Failed to update user");
     }
-
     return response.json();
   };
 
