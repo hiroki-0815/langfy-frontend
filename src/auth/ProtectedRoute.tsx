97664446e5ce2591f2f207a68b006468b0d16062
hiroki <haohuirixia851@gmail.com>
2025-01-09 10:38:30 +0900

    organize for socket.io

--- src/auth/ProtectedRoute.tsx
+++ src/auth/ProtectedRoute.tsx
@@ -1,18 +1,39 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import { Navigate, Outlet } from "react-router-dom";
+import { SocketProvider } from "@/context/SocketContext";
+import { useGetMyUser } from "@/api/MyUserApi";
+import Loading from "@/skeletons/Loading";
 
-const ProtectedRoute = () => {
-  const { isAuthenticated, isLoading } = useAuth0();
+type Props = {
+  needCurrentUser?: boolean;
+};
 
-  if (isLoading) {
-    return null;
+const ProtectedRoute = ({ needCurrentUser = false }: Props) => {
+  const { isAuthenticated, isLoading: isAuthLoading } = useAuth0();
+
+  // Fetch currentUser only when needed
+  const { currentUser, isLoading: isCurrentUserLoading } = needCurrentUser
+    ? useGetMyUser()
+    : { currentUser: null, isLoading: false };
+
+  // Show a loading indicator while authentication is loading
+  if (isAuthLoading || (needCurrentUser && isCurrentUserLoading)) {
+    return <Loading />;
   }
 
-  if (isAuthenticated) {
-    return <Outlet />;
+  // Redirect to the homepage if the user is not authenticated or if currentUser is required but missing
+  if (!isAuthenticated || (needCurrentUser && !currentUser)) {
+    return <Navigate to="/" replace />;
   }
 
-  return <Navigate to="/" replace />;
+  // Conditionally wrap the outlet with a SocketProvider if needed
+  return needCurrentUser ? (
+    <SocketProvider currentUser={currentUser || null}>
+      <Outlet />
+    </SocketProvider>
+  ) : (
+    <Outlet />
+  );
 };
 
 export default ProtectedRoute;
