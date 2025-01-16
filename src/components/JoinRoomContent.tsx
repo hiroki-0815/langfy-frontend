faa83c0d09f524a80dd362db5917cfd3096475b6
hiroki <haohuirixia851@gmail.com>
2025-01-16 11:00:20 +0900

    build join room page

--- src/components/JoinRoomContent.tsx
+++ src/components/JoinRoomContent.tsx
@@ -3,25 +3,53 @@ import JoinRoomInputs from "./video-call/JoinRoomInputs";
 import { RootState } from "@/store/store";
 import { connect } from "react-redux";
 import OnlyWithAudioCheckbox from "./video-call/OnlyWithAudioCheckbox";
+import { Dispatch } from "@reduxjs/toolkit";
+import { setConnectOnlyWithAudio, setIsRoomHost } from "@/store/actions";
+import JoinRoomButtons from "./video-call/JoinRoomButtons";
+// import ErrorMessage from "./video-call/ErrorMessage";
 
 type Props = {
   isRoomHost: boolean;
+  setConnectOnlyWithAudio: (onlyWithAudio: boolean) => void;
+  connectOnlyWithAudio: boolean;
 };
 
-const JoinRoomContent = ({ isRoomHost }: Props) => {
+const JoinRoomContent = ({
+  isRoomHost,
+  setConnectOnlyWithAudio,
+  connectOnlyWithAudio,
+}: Props) => {
   const [roomIdValue, setRoomIdValue] = useState("");
   const [nameValue, setNameValue] = useState("");
+  // const [errorMessage, setErrorMessage] = useState(null)
+  const handleJoinRoom = () => {
+    console.log("joining");
+  };
+
   return (
-    <>
-      <JoinRoomInputs
-        roomIdValue={roomIdValue}
-        setRoomIdValue={setRoomIdValue}
-        nameValue={nameValue}
-        setNameValue={setNameValue}
-        isRoomHost={isRoomHost}
-      />
-      <OnlyWithAudioCheckbox />
-    </>
+    <div className="bg-white rounded-lg">
+      <div className="mb-2">
+        <JoinRoomInputs
+          roomIdValue={roomIdValue}
+          setRoomIdValue={setRoomIdValue}
+          nameValue={nameValue}
+          setNameValue={setNameValue}
+          isRoomHost={isRoomHost}
+        />
+      </div>
+      <div className="mb-5">
+        <OnlyWithAudioCheckbox
+          connectOnlyWithAudio={connectOnlyWithAudio}
+          setConnectOnlyWithAudio={setConnectOnlyWithAudio}
+        />
+      </div>
+      <div>
+        <JoinRoomButtons
+          handleJoinRoom={handleJoinRoom}
+          isRoomHost={isRoomHost}
+        />
+      </div>
+    </div>
   );
 };
 
@@ -31,4 +59,15 @@ const mapStoreStateToProps = (state: RootState) => {
   };
 };
 
-export default connect(mapStoreStateToProps)(JoinRoomContent);
+const mapActionsToProps = (dispatch: Dispatch) => {
+  return {
+    setIsRoomHost: (isRoomHost: boolean) => dispatch(setIsRoomHost(isRoomHost)),
+    setConnectOnlyWithAudio: (onlyWithAudio: boolean) =>
+      dispatch(setConnectOnlyWithAudio(onlyWithAudio)),
+  };
+};
+
+export default connect(
+  mapStoreStateToProps,
+  mapActionsToProps
+)(JoinRoomContent);
