d29b2546ac19edb27c0948c1fb414e3994cc7a58
hiroki <haohuirixia851@gmail.com>
2025-01-17 09:09:16 +0900

    build room label

--- src/pages/RoomPage.tsx
+++ src/pages/RoomPage.tsx
@@ -1,5 +1,29 @@
+import ChatSection from "@/components/video-call/ChatSection";
+import ParticipantsSection from "@/components/video-call/ParticipantsSection";
+import RoomLabel from "@/components/video-call/RoomLabel";
+import VideoSection from "@/components/video-call/VideoSection";
+
 const RoomPage = () => {
-  return <div>RoomPage</div>;
+  return (
+    <div className="flex flex-col h-screen">
+      <div className="grid flex-1 grid-cols-[250px_1fr_250px]">
+        <div>
+          <ParticipantsSection />
+        </div>
+
+        <div>
+          <VideoSection />
+          <RoomLabel roomId="1234" />
+        </div>
+
+        <div>
+          <ChatSection />
+        </div>
+      </div>
+
+      <div></div>
+    </div>
+  );
 };
 
 export default RoomPage;
