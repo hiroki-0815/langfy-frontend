c3fd8fae0a05c689699bbd9a3e6eaf1b31952f96
hiroki <haohuirixia851@gmail.com>
2025-01-17 09:09:35 +0900

    build room label

--- src/store/actions.tsx
+++ src/store/actions.tsx
@@ -11,6 +11,8 @@ type Actions =
 const Actions = {
   SET_IS_ROOM_HOST: "SET_IS_ROOM_HOST",
   SET_CONNECT_ONLY_WITH_AUDIO: "SET_CONNECT_ONLY_WITH_AUDIO",
+  SET_ROOM_ID: "SET_ROOM_ID",
+  SET_IDENTITY: "SET_IDENTITY",
 };
 
 export const setIsRoomHost = (isRoomHost: boolean) => {
@@ -24,6 +26,20 @@ export const setConnectOnlyWithAudio = (onlyWithAudio: boolean) => {
   return {
     type: Actions.SET_CONNECT_ONLY_WITH_AUDIO,
     payload: onlyWithAudio,
+  };
+};
+
+export const setRoomId = (roomId: string) => {
+  return {
+    type: Actions.SET_ROOM_ID,
+    payload: roomId,
+  };
+};
+
+export const setIdentity = (identity: string) => {
+  return {
+    type: Actions.SET_IDENTITY,
+    payload: identity,
   };
 };
 
