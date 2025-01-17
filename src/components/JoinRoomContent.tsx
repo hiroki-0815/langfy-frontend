7b659853bd7e1ccab28cdd12961b39f20fd710fa
hiroki <haohuirixia851@gmail.com>
2025-01-17 09:09:00 +0900

    build room label

--- src/components/JoinRoomContent.tsx
+++ src/components/JoinRoomContent.tsx
@@ -4,27 +4,73 @@ import { RootState } from "@/store/store";
 import { connect } from "react-redux";
 import OnlyWithAudioCheckbox from "./video-call/OnlyWithAudioCheckbox";
 import { Dispatch } from "@reduxjs/toolkit";
-import { setConnectOnlyWithAudio, setIsRoomHost } from "@/store/actions";
+import {
+  setConnectOnlyWithAudio,
+  setIdentity,
+  setIsRoomHost,
+  setRoomId,
+} from "@/store/actions";
 import JoinRoomButtons from "./video-call/JoinRoomButtons";
-// import ErrorMessage from "./video-call/ErrorMessage";
+import ErrorMessage from "./video-call/ErrorMessage";
+import { useRoom } from "@/api/video-call/RoomApi";
+import { useNavigate } from "react-router-dom";
 
 type Props = {
   isRoomHost: boolean;
   setConnectOnlyWithAudio: (onlyWithAudio: boolean) => void;
   connectOnlyWithAudio: boolean;
+  setIdentityAction: (identity: string) => void;
+  setRoomIdAction: (roomId: string) => void;
 };
 
 const JoinRoomContent = ({
   isRoomHost,
   setConnectOnlyWithAudio,
   connectOnlyWithAudio,
+  setIdentityAction,
+  setRoomIdAction,
 }: Props) => {
   const [roomIdValue, setRoomIdValue] = useState("");
   const [nameValue, setNameValue] = useState("");
-  // const [errorMessage, setErrorMessage] = useState(null)
-  const handleJoinRoom = () => {
-    console.log("joining");
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
+  const { fetchRoom, isLoading } = useRoom();
+  const navigate = useNavigate();
+
+  const handleJoinRoom = async () => {
+    setIdentityAction(nameValue);
+    if (isRoomHost) {
+      createRoom();
+    } else {
+      await joinRoom();
+    }
   };
+
+  const joinRoom = async () => {
+    try {
+      const data = await fetchRoom(roomIdValue);
+      const { roomExists, full } = data;
+      if (roomExists) {
+        if (full) {
+          setErrorMessage("Meeting is full. Please try again later");
+        } else {
+          setRoomIdAction(roomIdValue);
+          navigate("/room");
+        }
+      } else {
+        setErrorMessage("Meeting not found. Check your meeting id");
+      }
+    } catch (error) {
+      setErrorMessage("Error connecting to the meeting");
+    }
+  };
+
+  const createRoom = () => {
+    navigate("/room");
+  };
+
+  if (isLoading) {
+    <div>...Loading</div>;
+  }
 
   return (
     <div className="bg-white rounded-lg">
@@ -42,6 +88,7 @@ const JoinRoomContent = ({
           connectOnlyWithAudio={connectOnlyWithAudio}
           setConnectOnlyWithAudio={setConnectOnlyWithAudio}
         />
+        <ErrorMessage errorMessage={errorMessage} />
       </div>
       <div>
         <JoinRoomButtons
@@ -64,6 +111,8 @@ const mapActionsToProps = (dispatch: Dispatch) => {
     setIsRoomHost: (isRoomHost: boolean) => dispatch(setIsRoomHost(isRoomHost)),
     setConnectOnlyWithAudio: (onlyWithAudio: boolean) =>
       dispatch(setConnectOnlyWithAudio(onlyWithAudio)),
+    setIdentityAction: (identity: string) => dispatch(setIdentity(identity)),
+    setRoomIdAction: (roomId: string) => dispatch(setRoomId(roomId)),
   };
 };
 
