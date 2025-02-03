85d7af6c2d7c2414544ecdbbd66f5eed77e1d275
hiroki <haohuirixia851@gmail.com>
2025-02-03 16:57:31 +0900

    build addIceCandidateToPeerConnection listener

--- src/pages/ProMainVideoPage.tsx
+++ src/pages/ProMainVideoPage.tsx
@@ -2,7 +2,6 @@ import ActionButtons from "@/components/video-call/ActionButtons";
 import { useEffect, useRef, useState } from "react";
 import { useAppDispatch, useAppSelector } from "../redux-elements/hooks";
 import addStream from "@/redux-elements/actions/addStream";
-import createPeerConnection from "@/WebRTCUutilities/createPeerConnection";
 import updateCallStatus, {
   setCallDescription,
 } from "@/redux-elements/actions/updateCallStatus";
@@ -11,24 +10,34 @@ import { useSearchParams, useLocation } from "react-router-dom";
 import { Clipboard, ClipboardCheck } from "lucide-react"; // Lucide icons for copy
 import { useGetMyUser } from "@/api/MyUserApi";
 import { useSelector } from "react-redux";
+import { useSocket } from "@/context/SocketContext";
+import { createPeerConnection } from "@/WebRTCUutilities/createPeerConnection";
+import calleeSocketListeners from "@/WebRTCUutilities/CalleeSocketListeners";
+import { StreamsType } from "@/redux-elements/type";
 
 const ProMainVideoPage = () => {
   const dispatch = useAppDispatch();
   const streams = useAppSelector((state: RootState) => state.stream);
   const smallFeedEl = useRef(null);
-  const largeFeedEl = useRef(null);
+  const largeFeedEl = useRef<HTMLVideoElement | null>(null);
   const [searchParams] = useSearchParams();
   const receiverId = searchParams.get("receiverId");
   const callStatus = useSelector((state: RootState) => state.callStatus);
+  const { socket } = useSocket();
 
   const { currentUser } = useGetMyUser();
   const isCaller = currentUser?._id !== receiverId;
 
-  const [copied, setCopied] = useState(false);
-  const [callerIsReady, setCallerIsReady] = useState(false);
-  const [calleeIsReady, setCalleeIsReady] = useState(false);
+  const [copied, setCopied] = useState<boolean>(false);
+  const [callerIsReady, setCallerIsReady] = useState<boolean>(false);
+  const [calleeIsReady, setCalleeIsReady] = useState<boolean>(false);
   const location = useLocation();
   const videoCallUrl = `${window.location.origin}${location.pathname}${location.search}`;
+
+  const offerIdRef = useRef<string | null>(null);
+
+  const [haveGottenIce, setHaveGottenIce] = useState<boolean>(false);
+  const streamsRef = useRef<StreamsType | null>(null);
 
   useEffect(() => {
     const fetchMedia = async () => {
@@ -37,8 +46,13 @@ const ProMainVideoPage = () => {
         const stream = await navigator.mediaDevices.getUserMedia(constraints);
         dispatch(updateCallStatus("haveMedia", true));
         dispatch(addStream("localStream", stream));
-        const { peerConnection, remoteStream } = await createPeerConnection();
+        const { peerConnection, remoteStream } = await createPeerConnection(
+          addIce
+        );
         dispatch(addStream("remote1", remoteStream, peerConnection));
+        if (largeFeedEl.current) {
+          largeFeedEl.current.srcObject = remoteStream;
+        }
       } catch (error) {
         console.log(error);
       }
@@ -46,6 +60,45 @@ const ProMainVideoPage = () => {
     fetchMedia();
     console.log("Fetching media and creating peer connections.");
   }, []);
+
+  useEffect(() => {
+    const fetchIceCandidates = async () => {
+      const offerId = offerIdRef.current;
+      if (!offerId) {
+        console.warn("⚠️ No offerId available to fetch ICE candidates.");
+        return;
+      }
+      if (!socket) {
+        console.warn("⚠️ Socket is not initialized.");
+        return;
+      }
+
+      try {
+        const iceCandidates = await socket.emitWithAck(
+          "getIce",
+          offerId,
+          "caller"
+        );
+        iceCandidates.forEach((iceC: RTCIceCandidate) => {
+          for (const s in streams) {
+            if (s !== "localStream") {
+              const peerConnection = streams[s].peerConnection;
+              peerConnection?.addIceCandidate(iceC);
+              console.log("=====Added Ice Candidate!!!");
+            }
+          }
+        });
+        console.log("✅ Received ICE candidates:", iceCandidates);
+      } catch (error) {
+        console.error("❌ Error fetching ICE candidates:", error);
+      }
+    };
+    if (streams.remote1 && !haveGottenIce) {
+      setHaveGottenIce(true);
+      fetchIceCandidates();
+      streamsRef.current = streams;
+    }
+  }, [streams, haveGottenIce]);
 
   useEffect(() => {
     const setOffer = async () => {
@@ -76,6 +129,9 @@ const ProMainVideoPage = () => {
       for (const s in streams) {
         if (s !== "localStream") {
           const peerConnection = streams[s].peerConnection;
+          const callerId = callStatus.callerId;
+          const offerId = callStatus.offerId;
+
           if (peerConnection) {
             try {
               const answer = await peerConnection.createAnswer({});
@@ -88,6 +144,9 @@ const ProMainVideoPage = () => {
               );
               dispatch(updateCallStatus("haveCreatedAnswer", true));
               dispatch(setCallDescription(answer));
+              console.log("Emitting newAnswer with offerId:", offerId);
+              console.log("Emitting newAnswer with callerId:", callerId);
+              socket?.emit("newAnswer", { answer, callerId, offerId });
             } catch (error) {
               console.error("Error creating or setting answer:", error);
             }
@@ -96,17 +155,44 @@ const ProMainVideoPage = () => {
       }
     };
     if (
-      (callStatus.audio === "enabled" && callStatus.video === "enabled",
-      !callStatus.haveCreatedAnswer)
+      callStatus.audio === "enabled" &&
+      callStatus.video === "enabled" &&
+      !callStatus.haveCreatedAnswer
     ) {
       createAnswer();
     }
-  }, [callStatus.audio && callStatus.video, callStatus.haveCreatedAnswer]);
+  }, [callStatus.audio, callStatus.video, callStatus.haveCreatedAnswer]);
+
+  useEffect(() => {
+    offerIdRef.current = callStatus.offerId;
+  }, [callStatus.offerId]);
+
+  useEffect(() => {
+    const addIceCandidateToPeerConnection = (iceC: RTCIceCandidate) => {
+      for (const s in streamsRef.current) {
+        if (s !== "localStream") {
+          const peerConnection = streamsRef.current[s].peerConnection;
+          peerConnection?.addIceCandidate(iceC);
+          console.log("add an icecandidate to existing page presence");
+        }
+      }
+    };
+    calleeSocketListeners(socket, addIceCandidateToPeerConnection);
+  }, []);
 
   const handleCopyLink = () => {
     navigator.clipboard.writeText(videoCallUrl);
     setCopied(true);
     setTimeout(() => setCopied(false), 2000);
+  };
+
+  const addIce = (iceC: RTCIceCandidate) => {
+    const offerId = offerIdRef.current;
+    if (!offerId) {
+      console.warn("Offer ID is not available yet.");
+      return;
+    }
+    socket?.emit("iceServer", { iceC, offerId, who: "callee" });
   };
 
   return (
@@ -171,7 +257,7 @@ const ProMainVideoPage = () => {
           playsInline
         ></video>
 
-        <ActionButtons smallFeedEl={smallFeedEl} />
+        <ActionButtons smallFeedEl={smallFeedEl} largeFeedEl={largeFeedEl} />
       </div>
     </div>
   );
