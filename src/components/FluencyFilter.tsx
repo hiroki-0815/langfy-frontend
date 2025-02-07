c36dfb20b09a3fa1bb2faf63916d989f4f7276c3
hiroki <haohuirixia851@gmail.com>
2025-02-07 17:12:41 +0900

    add translation

--- src/components/FluencyFilter.tsx
+++ src/components/FluencyFilter.tsx
@@ -5,23 +5,28 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "./ui/dropdown-menu";
+import { useTranslation } from "react-i18next";
 
 type Props = {
   onChange: (fluencyLevel: string) => void;
 };
 
 const FluencyFilter = ({ onChange }: Props) => {
+  const { t } = useTranslation();
+
   return (
     <div>
       <DropdownMenu>
         <DropdownMenuTrigger className="w-full text-left rounded-full bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-200">
-          Fluency
+          {t("fluencyLabel")}
         </DropdownMenuTrigger>
         <DropdownMenuContent className="bg-white border border-gray-200 rounded-lg shadow-lg">
-          <DropdownMenuItem onClick={() => onChange("")}>All</DropdownMenuItem>
+          <DropdownMenuItem onClick={() => onChange("")}>
+            {t("all")}
+          </DropdownMenuItem>
           {FLUENCY_LEVELS.map((level) => (
             <DropdownMenuItem key={level} onClick={() => onChange(level)}>
-              {level.charAt(0).toUpperCase() + level.slice(1)}
+              {t(`fluencyLevels.${level}`)}{" "}
             </DropdownMenuItem>
           ))}
         </DropdownMenuContent>
