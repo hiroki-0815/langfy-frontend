36c178af72784216b13e0b55b673096b780da91f
hiroki <haohuirixia851@gmail.com>
2025-02-08 15:46:43 +0900

    change the layout of the buttons

--- src/components/video-call/ActionButtons.tsx
+++ src/components/video-call/ActionButtons.tsx
@@ -21,22 +21,22 @@ const ActionButtons = ({
     <div
       className="
         fixed bottom-0 left-0 right-0 
-        flex flex-col md:flex-row 
-        items-center justify-between 
+        flex flex-col 
+        items-center justify-between
         bg-gray-800 shadow-md 
         transition-all duration-300 
         px-4 md:px-10 py-2 md:py-4
       "
     >
       {/* Audio & Video Controls */}
-      <div className="flex space-x-2 md:space-x-4">
+      <div className="flex gap-2">
         <AudioButton smallFeedEl={smallFeedEl} />
         <VideoButton smallFeedEl={smallFeedEl} />
-        <HangupButton smallFeedEl={smallFeedEl} largeFeedEl={largeFeedEl} />
         <TimerToggleButton
           onToggle={(newVisibility) => setIsTimerVisible(newVisibility)}
           currentVisibility={isTimerVisible}
         />
+        <HangupButton smallFeedEl={smallFeedEl} largeFeedEl={largeFeedEl} />
       </div>
     </div>
   );
